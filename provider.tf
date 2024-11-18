b95bcfdf4186912a28dae3ec62c0a20fd0f2e4ce
PaddyAdallah <adalapaddy9@gmail.com>
2024-11-18 17:05:08 +0000

    terraform migrate

---
+++ provider.tf
@@ -0,0 +1,19 @@
+terraform {
+    cloud {
+        organization = "PaddyAdallah"
+        workspaces {
+          name = "terraform-provision-eks-cluster"
+        }
+    required_providers {
+        aws = {
+          source  = "hashicorp/aws"
+          version = "~> 5.0"
+            }
+    }
+    
+    }
+}
+
+provider "aws" {
+  region = var.region
+}
